46a55eb4a4f8f0262c3846aeda5c1423036b0ea3
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2019-11-24 10:49:19 +0200

    Update ImageForm.cs

--- PopUp_Researcher/ImageForm.cs
+++ PopUp_Researcher/ImageForm.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using bRMS_Generator;
 using PopUp_Researcher.Helpers;
