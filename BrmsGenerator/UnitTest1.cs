b5f2c7ae432e9ef3c1c525cfbebadcda7d58ddbd
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-09-21 10:01:42 +0300

    Add unit testing project

---
+++ BrmsGenerator/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrmsGenerator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
